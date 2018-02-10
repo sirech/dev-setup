8b5d11f767f6d73c34d471e17d1a7b4e1027d989
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-02-10 12:07:58 +0100

    adapt datastores

--- datastores.sh
+++ datastores.sh
@@ -21,14 +21,9 @@ brew update
 # Install data stores
 brew install mysql
 brew install postgresql
-brew install mongo
-brew install redis
-brew install elasticsearch
-
-# Install mysql workbench
-# Install Cask
-brew install caskroom/cask/brew-cask
-brew cask install --appdir="/Applications" mysqlworkbench
+# brew install mongo
+# brew install redis
+# brew install elasticsearch
 
 # Remove outdated versions from the cellar.
 brew cleanup
