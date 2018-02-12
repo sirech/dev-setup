a10a38e62a8054c03a1852a5de97a5c2aaec6c61
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-02-12 21:58:48 +0100

    Add install ruby script

---
+++ ruby.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Install ruby.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+ruby_version=2.4.2
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
+CONFIGURE_OPTS='--with-readline-dir=/usr/include/readline' ~/.rbenv/bin/rbenv install ${ruby_version}
+~/.rbenv/bin/rbenv global ${ruby_version}
+~/.rbenv/shims/gem install bundle scss_lint_reporter_checkstyle
+sh ~/shell/irbrc/install.sh
