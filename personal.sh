b1acd5fb41bc5d3d421a4ad26d8ba5beb7ac284f
Mario Fernandez <mario.fernandez@hceris.com>
2020-05-19 12:41:22 +0200

    switch to develop spacemacs branch

--- personal.sh
+++ personal.sh
@@ -12,7 +12,7 @@ git clone https://github.com/zsh-users/zsh-completions.git --origin github ~/she
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
+git clone -b develop https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
 git clone https://github.com/sirech/spacemacs.d.git --origin github ~/.spacemacs.d
 
 git clone https://github.com/sirech/hammerspoon.git --origin github ~/.hammerspoon
