3cb359bc0be6cb26d9bca996e57f4a3ccd35f0bb
Mario Fernandez <mario.fernandez@hceris.com>
2020-06-16 23:38:51 +0200

    simplify rust install

--- rust.sh
+++ rust.sh
@@ -2,12 +2,6 @@
 
 # Install rust and libraries
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
@@ -18,7 +12,10 @@ fi
 # Make sure we’re using the latest Homebrew.
 brew update
 
-curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y
+if test ! $(which rustup); then
+  echo "Installing rustup..."
+  curl https://sh.rustup.rs -sSf | sh -s -- --no-modify-path -y
+fi
 rustup update
 
 # Remove outdated versions from the cellar.
